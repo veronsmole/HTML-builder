cd17a38b5d991e89168f234dce4374280556e211
veronsmole <veronsmole@gmail.com>
2021-11-03 15:51:39 +0300

    feat: add 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const path = require('path');
 
+const destDir = path.join(__dirname, 'files-copy');
+const sourceDir = path.join(__dirname, 'files');
+
+fs.rmdir(destDir, {recursive: true}, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  fs.mkdir(destDir,
+    {recursive: true}, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+      fs.readdir(sourceDir, (err, files) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        files.forEach(file => {
+          fs.copyFile(path.join(sourceDir, file), path.join(destDir, file), function (err) {
+            if (err !== null) {
+              console.error(err);
+            }
+          });
+        });
+      });
+    });
+});
